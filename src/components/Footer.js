d8a020502da8d83a55766bf77f65b5fbbe5d7280
duck <mayday0405@gmail.com>
2023-10-19 10:25:07 +0800

    react-route

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const footer = () => {
+  return (
+    <div className='footer'>2023 © duck made </div>
+  )
+}
+
+export default footer
---
+++ src/pages/Homepage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Search from "../components/Search";
+
+const Homepage = () => {
+  const auth = "RBiGauRZo3r7VzMFuwiXv8yq7a7qiREvkDEFhMAtZpGEnDj9M8eiNUbX";
+
+  return (
+    <div style={{ minHeight: "100vh" }}>
+      <Search />
+    </div>
+  );
+};
+
+export default Homepage;
