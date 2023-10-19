b9cf7bd27cdf0ead7421e8fbfe0630b44f4f1469
duck <mayday0405@gmail.com>
2023-10-19 17:44:42 +0800

    update search feature

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -5,12 +5,15 @@ import Picture from "../components/Picture";
 const Homepage = () => {
   const [input, setInput] = useState("");
   let [data, setData] = useState(null);
+  let [page, setPage] = useState(1);
+  let [currentSearch, setCurrentSearch] = useState("");
   const auth = "RBiGauRZo3r7VzMFuwiXv8yq7a7qiREvkDEFhMAtZpGEnDj9M8eiNUbX";
   const intialURL = "https://api.pexels.com/v1/curated?page=1&per_page=15";
-  const searchURL = `https://api.pexels.com/v1/search?query=${input}&per_page=15&page=1`;
+  const searchURL = `https://api.pexels.com/v1/search?query=${currentSearch}&per_page=15&page=1`;
 
   //fetch data from pixels api
   const search = async (url) => {
+    setPage(2);
     const dataFetch = await fetch(url, {
       method: "GET",
       headers: {
@@ -22,16 +25,45 @@ const Homepage = () => {
     setData(parsedData.photos);
   };
 
+  //Load more picture
+  const morepicture = async () => {
+    let newURL;
+    if (input === "") {
+      newURL = `https://api.pexels.com/v1/curated?page=${page}&per_page=15`;
+    } else {
+      newURL = `https://api.pexels.com/v1/search?query=${input}&per_page=15&page=${page}`;
+    }
+    setPage(page + 1);
+
+    const dataFetch = await fetch(newURL, {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+        Authorization: auth,
+      },
+    });
+    let parsedData = await dataFetch.json();
+    setData(data.concat(parsedData.photos));
+  };
+
   //fetch data when the page loads up
+  //useEffect(() => {
+  //  search(intialURL);
+  //}, []);
+
   useEffect(() => {
-    search(intialURL);
-  }, []);
+    if (currentSearch === "") {
+      search(intialURL);
+    } else {
+      search(searchURL);
+    }
+  }, [currentSearch]);
 
   return (
     <div style={{ minHeight: "100vh" }}>
       <Search
         search={() => {
-          search(searchURL);
+          setCurrentSearch(input);
         }}
         setInput={setInput}
       />
@@ -40,6 +72,10 @@ const Homepage = () => {
           data.map((d) => {
             return <Picture data={d} />;
           })}
+      </div>
+
+      <div className="morePicture">
+        <button onClick={morepicture}>Load More...</button>
       </div>
     </div>
   );
